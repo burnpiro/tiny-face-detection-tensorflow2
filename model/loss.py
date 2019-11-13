37d1e55384861edd2637205d237f3b616e01e11e
burnpiro <kemalpiro@gmail.com>
2019-11-13 16:56:11 +0100

    Default loss and validation function

---
+++ model/loss.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from config import cfg
+
+
+def detect_loss():
+    def get_box_highest_percentage(arr):
+        shape = tf.shape(arr)
+
+        reshaped = tf.reshape(arr, (shape[0], tf.reduce_prod(shape[1:-1]), -1))
+
+        # returns array containing the index of the highest percentage of each batch
+        # where 0 <= index <= height * width
+        max_prob_ind = tf.argmax(reshaped[..., -1], axis=-1, output_type=tf.int32)
+
+        # turn indices (batch, y * x) into (batch, y, x)
+        # returns (3, batch) tensor
+        unraveled = tf.unravel_index(max_prob_ind, shape[:-1])
+
+        # turn tensor into (batch, 3) and keep only (y, x)
+        unraveled = tf.transpose(unraveled)[:, 1:]
+        y, x = unraveled[..., 0], unraveled[..., 1]
+
+        # stack indices and create (batch, 5) tensor which
+        # contains height, width, offset_y, offset_x, percentage
+        indices = tf.stack([tf.range(shape[0]), y, x], axis=-1)
+        box = tf.gather_nd(arr, indices)
+
+        y, x = tf.cast(y, tf.float32), tf.cast(x, tf.float32)
+
+        # transform box to (y + offset_y, x + offset_x, 7 * height, 7 * width, obj)
+        # output is (batch, 5)
+        out = tf.stack([y + box[..., 2], x + box[..., 3],
+                        cfg.NN.GRID_SIZE * box[..., 0], cfg.NN.GRID_SIZE * box[..., 1],
+                        box[..., -1]], axis=-1)
+
+        return out
+
+    def loss(y_true, y_pred):
+        # get the box with the highest percentage in each image
+        true_box = get_box_highest_percentage(y_true)
+        pred_box = get_box_highest_percentage(y_pred)
+
+        # object loss
+        obj_loss = tf.keras.losses.binary_crossentropy(y_true[..., 4:5], y_pred[..., 4:5])
+
+        # mse with the boxes that have the highest percentage
+        box_loss = tf.reduce_sum(tf.math.squared_difference(true_box[..., :-1], pred_box[..., :-1]))
+
+        return tf.reduce_sum(obj_loss) + box_loss
+
+    return loss
---
+++ model/validation.py
@@ -0,0 +1,59 @@
+import os
+import numpy as np
+import tensorflow as tf
+from config import cfg
+
+
+class Validation(tf.keras.callbacks.Callback):
+    def get_box_highets_percentage(self, mask):
+        reshaped = mask.reshape(mask.shape[0], np.prod(mask.shape[1:-1]), -1)
+
+        score_ind = np.argmax(reshaped[..., -1], axis=-1)
+        unraveled = np.array(np.unravel_index(score_ind, mask.shape[:-1])).T[:, 1:]
+
+        cell_y, cell_x = unraveled[..., 0], unraveled[..., 1]
+        boxes = mask[np.arange(mask.shape[0]), cell_y, cell_x]
+
+        h, w, offset_y, offset_x = boxes[..., 0], boxes[..., 1], boxes[..., 2], boxes[..., 3]
+
+        return np.stack([cell_y + offset_y, cell_x + offset_x,
+                         cfg.NN.GRID_SIZE * h, cfg.NN.GRID_SIZE * w], axis=-1)
+
+    def __init__(self, generator):
+        self.generator = generator
+
+    def on_epoch_end(self, epoch, logs):
+        mse = 0
+        intersections = 0
+        unions = 0
+
+        for i in range(len(self.generator)):
+            batch_images, gt = self.generator[i]
+            pred = self.model.predict_on_batch(batch_images).numpy()
+
+            pred = self.get_box_highets_percentage(pred)
+            gt = self.get_box_highets_percentage(gt)
+
+            mse += np.linalg.norm(gt - pred, ord='fro') / pred.shape[0]
+
+            pred = np.maximum(pred, 0)
+            gt = np.maximum(gt, 0)
+
+            diff_height = np.minimum(gt[:,0] + gt[:,2], pred[:,0] + pred[:,2]) - np.maximum(gt[:,0], pred[:,0])
+            diff_width = np.minimum(gt[:,1] + gt[:,3], pred[:,1] + pred[:,3]) - np.maximum(gt[:,1], pred[:,1])
+            intersection = np.maximum(diff_width, 0) * np.maximum(diff_height, 0)
+
+            area_gt = gt[:,2] * gt[:,3]
+            area_pred = pred[:,2] * pred[:,3]
+            union = np.maximum(area_gt + area_pred - intersection, 0)
+
+            intersections += np.sum(intersection * (union > 0))
+            unions += np.sum(union)
+
+        iou = np.round(intersections / (unions + tf.keras.backend.epsilon()), 4)
+        logs["val_iou"] = iou
+
+        mse = np.round(mse, 4)
+        logs["val_mse"] = mse
+
+        print(" - val_iou: {} - val_mse: {}".format(iou, mse))
