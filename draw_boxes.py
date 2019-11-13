c7ee16c39561696619f1062f575d8ae3e1a13fa9
burnpiro <kemalpiro@gmail.com>
2019-11-13 18:25:17 +0100

    fix printing the score

--- draw_boxes.py
+++ draw_boxes.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def draw_outputs(img, boxes, draw_labels=True):
-    for y_c, x_c, h, w, _ in boxes:
+    for y_c, x_c, h, w, score in boxes:
         x0 = img.shape[1] * (x_c - w / 2)
         y0 = img.shape[0] * (y_c - h / 2)
         x1 = x0 + img.shape[1] * w
@@ -13,6 +13,6 @@ def draw_outputs(img, boxes, draw_labels=True):
         img = cv2.rectangle(img, x1y1, x2y2, (255, 0, 0), 2)
         if draw_labels:
             img = cv2.putText(img, '{} {:.4f}'.format(
-                'human', w),
+                'human', score),
                               x1y1, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
     return img
